df3fcdc33c3ad73316d6910910b36424776dbfa5
Ygor Mattos <ygormattos.b@gmail.com>
2020-03-23 18:31:46 -0300

    :wastebasket: Deprecation files

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
-  return res.send('ok')
+  return res.render('about')
 })
 
 module.exports = routes
