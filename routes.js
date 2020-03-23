cecc931e1e7a1138f586d57aefa4ebf5765bc1a2
Ygor Mattos <ygormattos.b@gmail.com>
2020-03-23 18:41:57 -0300

    :recycle: Refactoring views

--- routes.js
+++ routes.js
@@ -2,7 +2,13 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
-  return res.render('about')
+  return res.redirect("/instructos")
+})
+routes.get('/instructos', (req, res) => {
+  return res.render('instructos/index')
+})
+routes.get('/members', (req, res) => {
+  return res.send('members')
 })
 
 module.exports = routes
