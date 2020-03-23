32ae13519cdf07e737edc0e5ec41c9479b4cfc52
Ygor Mattos <ygormattos.b@gmail.com>
2020-03-23 18:26:03 -0300

    :tada: Init project

---
+++ public/scripts.js
@@ -0,0 +1,16 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards) {
+  card.addEventListener("click", () => {
+    const videoId = card.getAttribute("id")
+    window.location.href = `/video?id=${videoId}`
+
+  })
+}
+
+document.querySelector('.close-modal').addEventListener("click", () => {
+  modalOverlay.classList.remove('active')
+  iframe.src = ""
+})
+
