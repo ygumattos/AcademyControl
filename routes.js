c3dee47c286495d35950374e9962b536e0cfc783
Ygor Mattos <ygormattos.b@gmail.com>
2020-03-30 18:37:03 -0300

    Styling

--- routes.js
+++ routes.js
@@ -2,11 +2,21 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
-  return res.redirect("/instructos")
+  return res.redirect("/instructors")
 })
-routes.get('/instructos', (req, res) => {
-  return res.render('instructos/index')
+
+routes.get('/instructors', (req, res) => {
+  return res.render('instructors/index')
 })
+
+routes.post('/instructors', (req, res) => {
+  return res.send('recebido')
+})
+
+routes.get('/instructors/create', (req, res) => {
+  return res.render('instructors/create')
+})
+
 routes.get('/members', (req, res) => {
   return res.send('members')
 })
